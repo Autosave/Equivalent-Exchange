cff17acba975a5adce1bc3695b143b9aa52f99dc
Justin West <justinraywest@mail.com>
2015-04-21 13:53:05 -0500

    added tooltips to starting items

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
-	$("header div").append("<a class='location' href='#'>Bedroom</a>");
-	$("header div").append("<a class='location' href='#'>Cern</a>");
-	$("#inventory").append("<p><a href='#'>Book of Formulas</a></p>");
+	// Appends the starting location to the header.
+	$("header div").append("<a href='#' class='location'>Bedroom</a>");
+	$("header div").append("<a href='#' class='location'>Cern</a>");
 
+	// Appends the starting items to the inventory.
+	$("#inventory").append("<p><a href='#' class='items' " +
+						   "title='A blank book for writing alchemic formulas.'>Book of Formulas</a></p>");
+	$("#inventory").append("<p><a href='#' class='items' " +
+						   "title='A standard compass used for determining the direction.'>Compass</a></p>");
+
+	// Needed for differentiating between present location and other discovered locations.
 	$('.location').on('click', function() {
 		$('.location').removeClass('current-location');
 		$(this).addClass('current-location');
 	});
+
+	// Provides tooltip functionality to items in the inventory.
+	$('.items').tooltipster({
+		theme: 'tooltipster-noir'
+	});
+
 });
+
 
 
 
