9e49b295a8dada6cf4b3397488081e7dd6ccff41
Justin West <justinraywest@mail.com>
2015-04-24 18:35:55 -0500

    reconstructed utilizing bootstrap and sass

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
+	
 	// Appends the starting location to the header.
-	$("header div").append("<a href='#' class='location'>Bedroom</a>");
-	$("header div").append("<a href='#' class='location'>Cern</a>");
+	$("header").append("<a href='#' class='location'>Bedroom</a>");
+	$("header").append("<a href='#' class='location'>Cern</a>");
 
 	// Appends the starting items to the inventory.
-	$("#inventory").append("<p><a href='#' class='items' " +
-						   "title='A blank book for writing alchemic formulas.'>Book of Formulas</a></p>");
-	$("#inventory").append("<p><a href='#' class='items' " +
-						   "title='A standard compass used for determining the direction.'>Compass</a></p>");
+	$("#inventory").append("<p class='items' " +
+						   "title='A blank book for writing alchemic formulas.'>Book of Formulas</p>");
+	$("#inventory").append("<p class='items' " +
+						   "title='A standard compass used for determining the direction.'>Compass</p>");
 
 	// Needed for differentiating between present location and other discovered locations.
 	$('.location').on('click', function() {
 		$('.location').removeClass('current-location');
 		$(this).addClass('current-location');
+		alert("Working!");
+	});
+
+	// Needed for actionbar action specific options to appear.
+	$('#left-circle').click(function() {
+		$('.alchemy-options').toggle();
+		alert("Working!");
 	});
 
 	// Provides tooltip functionality to items in the inventory.
